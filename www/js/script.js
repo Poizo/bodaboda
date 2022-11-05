63515789e02f24c12f42879d198d2d9a6adf9f77
Poizo <poizo.artist@gmail.com>
2022-11-05 10:15:16 +0100

    font for designing head of invitation + browser support

--- www/js/script.js
+++ www/js/script.js
@@ -16,6 +16,54 @@ window.onload = () => {
     }
     isInvitedAllDay = slug === '/boda';
 
+
+
+    const checkSafariBrowser = () => {
+        const userAgent = navigator.userAgent.toLowerCase();
+        if (/.*safari.*/.test(userAgent) && !/.*chrome.*/.test(userAgent)) {
+            alert(`
+                It looks like you're using Safari Browser, we invite you to switch to another browser like 'Google Chrome' for example. It will provide you a better experience, because Safari doesn't fully support every feature of this web application.
+
+                Il semblerait que vous utilisiez le navigateur Safari, nous vous invitons à passer sur un autre navigateur comme 'Google Chrome' par exemple. Cela vous offrira une meilleure expérience, car Safari ne prend pas entièrement en charge toutes les fonctionnalités de cette application Web.
+
+                Het lijkt erop dat je de Safari browser gebruikt, wij schakelen liever over naar een andere browser zoals bijvoorbeeld 'Google Chrome'. Dit geeft u een betere ervaring, aangezien Safari niet alle functies van deze webtoepassing volledig ondersteunt.
+            `);
+        }
+    }
+
+    const switchLang = (lang) => {
+        currentLang = lang;
+        const otherLang = lang === 'fr' ? 'nl' : 'fr';
+        mainInvitation.classList.add(`-${lang}`);
+        mainInvitation.classList.remove(`-${otherLang}`);
+        menuOpener.classList.add(`-${lang}`);
+        menuOpener.classList.remove(`-${otherLang}`);
+        document.querySelector(`[data-lang="${lang}"]`).classList.add('-active');
+        document.querySelector(`[data-lang="${otherLang}"]`).classList.remove('-active');
+        document.querySelectorAll(`.lang__${otherLang}`).forEach(el => {
+            el.classList.add('hidden');
+        });
+        document.querySelectorAll(`.lang__${lang}`).forEach(el => {
+            el.classList.remove('hidden');
+        });
+        const htmlPage = document.getElementsByTagName('html')[0];
+        htmlPage.lang = currentLang;
+        document.querySelector('head title').textContent = currentLang === 'fr' ? frTitle : nlTitle;
+    }
+
+    const chooseLanguage = (lang) => {
+        switchLang(lang);
+        document.querySelector('.language-selection-page').classList.add('-disappear');
+        mainInvitation.classList.remove('-invisible');
+        document.querySelector('.aside-menu').classList.remove('hidden');
+    }
+
+
+    
+    ///////////////////////////
+
+    checkSafariBrowser();
+
     ///////////////////////////
     document.querySelector('.choose-language-button.-fr').addEventListener('click', () => {
         chooseLanguage('fr');
@@ -51,31 +99,7 @@ window.onload = () => {
             switchLang(btn.dataset.lang);
         });
     });
+
     //////////////////////
-
-
-    const switchLang = (lang) => {
-        currentLang = lang;
-        mainInvitation.classList.add(`-${lang}`);
-        mainInvitation.classList.remove(`-${lang === 'fr' ? 'nl' : 'fr'}`);
-        menuOpener.classList.add(`-${lang}`);
-        menuOpener.classList.remove(`-${lang === 'fr' ? 'nl' : 'fr'}`);
-        document.querySelectorAll(`.lang__${lang === 'nl' ? 'fr' : 'nl'}`).forEach(el => {
-            el.classList.add('hidden');
-        });
-        document.querySelectorAll(`.lang__${lang === 'fr' ? 'fr' : 'nl'}`).forEach(el => {
-            el.classList.remove('hidden');
-        });
-        const htmlPage = document.getElementsByTagName('html')[0];
-        htmlPage.lang = currentLang;
-        document.querySelector('head title').textContent = currentLang === 'fr' ? frTitle : nlTitle;
-    }
-
-    const chooseLanguage = (lang) => {
-        switchLang(lang);
-        document.querySelector('.language-selection-page').classList.add('-disappear');
-        mainInvitation.classList.remove('-invisible');
-        document.querySelector('.aside-menu').classList.remove('hidden');
-    }
 
 }
